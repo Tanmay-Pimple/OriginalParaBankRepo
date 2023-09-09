31bca9c646c6d225add59857c0b474884a6632b0
KANCHAN <KANCHAN@LAPTOP-CCT0SN8F>
2023-09-09 17:49:56 +0530

    2nd commit

--- 09_09_23-ParaBankCucumberMavenGithubProject/src/test/java/stepDefinitionpkg/ParaBankLoginClass.java
+++ 09_09_23-ParaBankCucumberMavenGithubProject/src/test/java/stepDefinitionpkg/ParaBankLoginClass.java
@@ -1,6 +1,8 @@
 package stepDefinitionpkg;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
@@ -9,23 +11,39 @@ import cucumber.api.java.en.When;
 
 public class ParaBankLoginClass {
 	
+	public WebDriver d;
+	
 	@Given("^Setup and Launch Site$")
 	public void setup_and_Launch_Site() throws Throwable 
 	{
-	 WebDriver d = new ChromeDriver();
+	 d = new ChromeDriver();
 	 d.get("https://parabank.parasoft.com/parabank/index.htm");
 	}
 
 	@When("^Enter UserName and Password$")
 	public void enter_UserName_and_Password() throws Throwable 
 	{
-	    System.out.println("Browser Launched");
+	    WebElement un = d.findElement(By.xpath("//*[@id=\'loginPanel\']/form/div[1]/input"));
+	    un.click();
+	    un.sendKeys("Vihaankahere");
+	    
+	    WebElement pwd = d.findElement(By.xpath("//*[@id=\'loginPanel\']/form/div[2]/input"));
+	    pwd.click();
+	    pwd.sendKeys("Pass@123");
 	}
 
 	@Then("^Click on Login Button$")
 	public void click_on_Login_Button() throws Throwable 
 	{
-		System.out.println("Website Launched");
+		WebElement login = d.findElement(By.xpath("//*[@id=\'loginPanel\']/form/div[3]/input"));
+		login.click();
+		
+		String pgttl = d.getTitle();
+		
+		System.out.println(pgttl);
+		
+		WebElement logout = d.findElement(By.xpath("//*[@id=\'leftPanel\']/ul/li[8]/a"));
+		logout.click();
 	}
 
 }
